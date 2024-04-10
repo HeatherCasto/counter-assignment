ceee37c5ca585e5b82fcbf58b6577a5e53792680
HeatherCasto <heathercasto101@gmail.com>
2024-04-09 22:31:30 -0400

    Working Unit Tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,31 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
-})
+  render(<Counter />);
+});
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByText(/Count/i);
+  expect(initialCount).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const addButton = screen.getByRole('button', { name: '+' });
+  fireEvent.click(addButton);
+  const countAfterIncrement = screen.getByTestId('count');
+  expect(countAfterIncrement).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const subtractButton = screen.getByRole('button', { name: '-' });
+  fireEvent.click(subtractButton);
+  const countAfterDecrement = screen.getByTestId('count');
+  expect(countAfterDecrement).toHaveTextContent('-1');
 });
